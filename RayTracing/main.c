72867602f298e9a5ab54d6768a61afab14fbed4d
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-12-22 11:15:44 +0530

    raytracing-4

--- RayTracing/main.c
+++ RayTracing/main.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 
-#define WIDTH  1600
+#define WIDTH  1000
 #define HEIGHT 800
 
 #define COLOR_EARTH (SDL_Color){0, 0, 255, 255}
 #define COLOR_SUN   (SDL_Color){255, 200, 50, 255}
+#define COLOR_RAY   (SDL_Color){255, 180, 80, 255}
 
 typedef struct Circle {
     double x;
@@ -15,37 +16,63 @@ typedef struct Circle {
 } Circle;
 
 
-typedef struct Shodow {
-    double x;
-    double y;
-    double radius;
-} Sun;
-
-void rays(Circle earth, Sun sun) {
-    
-
+double dist(double x1, double y1, double x2, double y2) {
+    double dx = x1 - x2;
+    double dy = y1 - y2;
+    return sqrt(dx * dx + dy * dy);
 }
 
-void FillCircle(SDL_Surface *surface, Circle circle, SDL_Color color) {
-    Uint32 pixelColor = SDL_MapRGB(
-        surface->format, color.r, color.g, color.b
-    );
+void FillCircle(SDL_Surface *surface, Circle c, SDL_Color color) {
+    Uint32 col = SDL_MapRGB(surface->format, color.r, color.g, color.b);
 
     SDL_LockSurface(surface);
-
     Uint32 *pixels = (Uint32 *)surface->pixels;
     int pitch = surface->pitch / 4;
 
     for (int y = 0; y < surface->h; y++) {
         for (int x = 0; x < surface->w; x++) {
-
-            double dx = x - circle.x;
-            double dy = y - circle.y;
-            double distance = sqrt(dx * dx + dy * dy);
-
-            if (distance <= circle.radius) {
-                pixels[y * pitch + x] = pixelColor;
+            if (dist(x, y, c.x, c.y) <= c.radius) {
+                pixels[y * pitch + x] = col;
             }
+        }
+    }
+
+    SDL_UnlockSurface(surface);
+}
+
+void DrawSunRays(SDL_Surface *surface, Circle sun, Circle earth) {
+
+    Uint32 rayCol = SDL_MapRGB(
+        surface->format,
+        COLOR_RAY.r, COLOR_RAY.g, COLOR_RAY.b
+    );
+
+    SDL_LockSurface(surface);
+    Uint32 *pixels = (Uint32 *)surface->pixels;
+    int pitch = surface->pitch / 4;
+
+    int ray_count = 720;
+    double max_len = 900.0;
+
+    for (int i = 0; i < ray_count; i++) {
+        double angle = (2.0 * M_PI * i) / ray_count;
+        double dx = cos(angle);
+        double dy = sin(angle);
+
+        for (double t = sun.radius; t < max_len; t += 1.0) {
+
+            int x = (int)(sun.x + dx * t);
+            int y = (int)(sun.y + dy * t);
+
+            if (x < 0 || x >= surface->w ||
+                y < 0 || y >= surface->h)
+                break;
+
+            
+            if (dist(x, y, earth.x, earth.y) <= earth.radius)
+                break;
+
+            pixels[y * pitch + x] = rayCol;
         }
     }
 
@@ -54,43 +81,30 @@ void FillCircle(SDL_Surface *surface, Circle circle, SDL_Color color) {
 
 int main(int argc, char *argv[]) {
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
+    SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *window = SDL_CreateWindow(
-        "Sun & Earth (Original Mouse Logic)",
+        "Sharp Rays + Draggable Sun & Earth",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        WIDTH,
-        HEIGHT,
+        WIDTH, HEIGHT,
         SDL_WINDOW_SHOWN
     );
 
-    if (!window) {
-        printf("Window Error: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
     SDL_Surface *surface = SDL_GetWindowSurface(window);
-    if (!surface) {
-        printf("Surface Error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
 
-    
-    Circle earth = {200, 200, 80};
-    Sun sun      = {500, 400, 140};
+    Circle sun   = {500, 400, 140};
+    Circle earth = {750, 400, 80};
 
     int running = 1;
+    int dragging_sun = 0;
+    int dragging_earth = 0;
+
     SDL_Event event;
 
     while (running) {
         while (SDL_PollEvent(&event)) {
+
             if (event.type == SDL_QUIT)
                 running = 0;
 
@@ -99,30 +113,49 @@ int main(int argc, char *argv[]) {
                 running = 0;
 
             
-            if (event.type == SDL_MOUSEMOTION && event.motion.state != 0) {
-                earth.x = event.motion.x;
-                earth.y = event.motion.y;
+            if (event.type == SDL_MOUSEBUTTONDOWN &&
+                event.button.button == SDL_BUTTON_LEFT) {
+
+                double d_sun   = dist(event.button.x, event.button.y, sun.x, sun.y);
+                double d_earth = dist(event.button.x, event.button.y, earth.x, earth.y);
+
+                if (d_sun <= sun.radius)
+                    dragging_sun = 1;
+                else if (d_earth <= earth.radius)
+                    dragging_earth = 1;
+            }
+
+            
+            if (event.type == SDL_MOUSEBUTTONUP &&
+                event.button.button == SDL_BUTTON_LEFT) {
+                dragging_sun = 0;
+                dragging_earth = 0;
+            }
+
+            
+            if (event.type == SDL_MOUSEMOTION &&
+                event.motion.state & SDL_BUTTON_LMASK) {
+
+                if (dragging_sun) {
+                    sun.x = event.motion.x;
+                    sun.y = event.motion.y;
+                }
+                else if (dragging_earth) {
+                    earth.x = event.motion.x;
+                    earth.y = event.motion.y;
+                }
             }
         }
 
-        SDL_FillRect(
-            surface,
-            NULL,
-            SDL_MapRGB(surface->format, 0, 0, 0)
-        );
+        SDL_FillRect(surface, NULL,
+            SDL_MapRGB(surface->format, 0, 0, 0));
 
-        
-        FillCircle(
-            surface,
-            (Circle){sun.x, sun.y, sun.radius},
-            COLOR_SUN
-        );
-
-        
+        DrawSunRays(surface, sun, earth);
+        FillCircle(surface, sun, COLOR_SUN);
         FillCircle(surface, earth, COLOR_EARTH);
 
         SDL_UpdateWindowSurface(window);
-        SDL_Delay(10);
+        SDL_Delay(16);
     }
 
     SDL_DestroyWindow(window);
